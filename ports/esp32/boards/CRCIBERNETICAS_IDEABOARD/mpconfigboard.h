3a1ce1e5d536513a08822cddb2db563765fa1dea
emanuel-mena <emena9215@gmail.com>
2025-02-27 16:25:06 -0600

    Typo Arreglado

--- ports/esp32/boards/CRCIBERNETICAS_IDEABOARD/mpconfigboard.h
+++ ports/esp32/boards/CRCIBERNETICAS_IDEABOARD/mpconfigboard.h
@@ -2,7 +2,7 @@
 // specified.
 
 #ifndef MICROPY_HW_BOARD_NAME
-#define MICROPY_HW_BOARD_NAME "GCRCibernetica's IdeaBoard"
+#define MICROPY_HW_BOARD_NAME "CRCibernetica's IdeaBoard"
 #endif
 
 #ifndef MICROPY_HW_MCU_NAME
